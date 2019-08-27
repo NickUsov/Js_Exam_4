7def61d66d2612cdbd3eadac5b4c18d32d5e290e
Nick Usov <nicky.yalta.ua@gmail.com>
2019-08-27 22:17:20 +0300

    ups

--- script.js
+++ script.js
@@ -11,12 +11,21 @@ $('document').ready(()=>{
         let id = $(event.currentTarget).attr("id");
         let txt = $(`label[for ='${id}']`).text();
         let par = $(event.currentTarget).parent();
-        $(par).html(`<span><s>${txt}</s></span><div class="circle">X</div></div>`);
+        $(par).html(`<span ondblclick="doubleclickSpan()"><s>${txt}</s></span><div class="circle">X</div></div>`);
     });
 
     $('body').on("click", ".circle", (event)=>{
         let par = $(event.currentTarget).parent();
         let id = $(par).attr("id");
         $(`#${id}`).remove();
-    })
+    });
+
+    $('body').on("dblclick", "span", ()=> {
+        let txt = $(event.currentTarget).text();
+        txt = txt.substring(0, txt.length - 1);
+        let par = $(event.currentTarget).parent();
+        let id = $(par).attr("id");
+        //$(par).html(`<input type="text" class="input_text">${txt}</input><div class="circle">X</div>`)
+        console.log(event.currentTarget);
+    });
 });
